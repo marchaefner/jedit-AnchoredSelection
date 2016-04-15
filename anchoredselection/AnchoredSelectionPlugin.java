4397cc6d61483efe999c36546329586aa7aa3d14
Marc Häfner <mhaefner@zoho.com>
2016-04-15 21:47:07 +0200

    The Great Refactor: consolidate main plugin class
      * separate buffer/caret handling from listener boilerplate
      * move stuff around so it looks better
      * add comments

--- anchoredselection/AnchoredSelectionPlugin.java
+++ anchoredselection/AnchoredSelectionPlugin.java
@@ -11,8 +11,6 @@ import org.gjt.sp.jedit.textarea.TextArea;
 import org.gjt.sp.jedit.textarea.Selection;
 import org.gjt.sp.jedit.buffer.JEditBuffer;
 import org.gjt.sp.jedit.buffer.BufferAdapter;
-import org.gjt.sp.jedit.buffer.BufferListener;
-import org.gjt.sp.jedit.EditBus.EBHandler;
 import org.gjt.sp.jedit.msg.EditPaneUpdate;
 import org.gjt.sp.jedit.msg.ViewUpdate;
 import org.gjt.sp.jedit.msg.PropertiesChanged;
@@ -28,55 +26,21 @@ public class AnchoredSelectionPlugin extends EditPlugin {
     public static final String NAME = "anchoredselection";
     public static final String OPTION_PREFIX = "options.anchoredselection.";
 
-    static AnchorMap anchorMap;
+    // {{{ Data structures & handler scaffolding
 
-    @Override
-    public void start()	{
-        anchorMap = new AnchorMap();
-        Actions.overrideBuiltInActions();
-        StatusBarWidgetManager.start();
-        EditBus.addToBus(this);
-    }
-
-    @Override
-    public void stop()	{
-        EditBus.removeFromBus(this);
-        StatusBarWidgetManager.stop();
-        Actions.removeOverriddenActions();
-        caretHandler.removeAll();
-        bufferHandler.removeAll();
-    }
-
-    static Set<TextArea> skipCaretUpdate =
-                Collections.newSetFromMap(new WeakHashMap<TextArea, Boolean>());
-
-    static Handler caretHandler = new Handler<TextArea>() {
+    /* anchorMap holds the anchor position for each buffer in each text area */
+    private static AnchorMap anchorMap = new AnchorMap();
+    /* text areas for which the next caret update should be ignored */
+    private static Set<TextArea> skipCaretUpdate = Collections.newSetFromMap(
+                                        new WeakHashMap<TextArea, Boolean>());
+    // {{{ Handler classes
+    // build Handlers for easier adding/removal - for actual handling see below
+    private static Handler<TextArea> caretHandler = new Handler<TextArea>() {
         CaretListener listener = new CaretListener() {
             public void caretUpdate(CaretEvent event) {
-                try {
-                    resizeSelection((TextArea)event.getSource());
-                } catch(ClassCastException ex) {}
+                handleCaretUpdate((TextArea)event.getSource());
             }
         };
-
-        void resizeSelection(TextArea textArea) {
-            if(!skipCaretUpdate.remove(textArea)) {
-                Integer anchor = anchorMap.get(textArea);
-                if(anchor != null) {
-                    int caret = textArea.getCaretPosition();
-                    Selection selection = textArea.getSelectionAtOffset(caret);
-                    if(selection != null
-                            && selection.getStart() == Math.min(caret, anchor)
-                            && selection.getEnd() == Math.max(caret,anchor)) {
-                        return;
-                    }
-                    skipCaretUpdate(textArea);
-                    textArea.resizeSelection(anchor, caret, 0,
-                                    textArea.isRectangularSelectionEnabled());
-                }
-            }
-        }
-
         void addListener(TextArea textArea) {
             textArea.addCaretListener(listener);
         }
@@ -85,15 +49,11 @@ public class AnchoredSelectionPlugin extends EditPlugin {
         }
     };
 
-    static Handler bufferHandler = new Handler<JEditBuffer>() {
-        BufferListener listener = new BufferAdapter() {
+    private static Handler<JEditBuffer> bufferHandler = new Handler<JEditBuffer>() {
+        BufferAdapter listener = new BufferAdapter() {
             public void preContentRemoved(JEditBuffer buffer, int startLine,
-                                            int offset, int numLines, int length) {
-                anchorMap.remove(buffer, offset, length);
-                if(!anchorMap.contains(buffer)) {
-                    bufferHandler.removeFrom(buffer);
-                }
-                StatusBarWidgetManager.updateAllWidgets();
+                                            int offset, int lines, int length) {
+                handlePreContentRemoved(buffer, offset, length);
             }
         };
         void addListener(JEditBuffer buffer) {
@@ -103,8 +63,72 @@ public class AnchoredSelectionPlugin extends EditPlugin {
             buffer.removeBufferListener(listener);
         }
     };
+    // }}} }}}
 
-    @EBHandler
+    // {{{ Plug in startup / teardown
+
+    /** Override built-in actions and add status bar widgets */
+    public void start()	{
+        Actions.overrideBuiltInActions();
+        StatusBarWidgetManager.start();
+        EditBus.addToBus(this);
+    }
+
+    /** Remove all listeners, overridden actions and status bar widgets. */
+    public void stop()	{
+        EditBus.removeFromBus(this);
+        StatusBarWidgetManager.stop();
+        Actions.removeOverriddenActions();
+        caretHandler.removeAll();
+        bufferHandler.removeAll();
+    }
+    // }}}
+
+    // {{{ Handler methods
+
+    /**
+     *  Re-set selection from the anchor to the new caret position.
+     *
+     *  Don't do anything if the selection is already as it should be (and
+     *  naturally if no anchor exists for the current buffer in the current text
+     *  area has no anchor set or skipCaretUpdate has been explicitly set).
+     */
+    private static void handleCaretUpdate(TextArea textArea) {
+        if(skipCaretUpdate.remove(textArea) || !anchorMap.contains(textArea)) {
+            return;
+        }
+        int anchor = anchorMap.get(textArea);
+        int caret = textArea.getCaretPosition();
+        Selection selection = textArea.getSelectionAtOffset(caret);
+        if(selection != null
+                && selection.getStart() == Math.min(caret, anchor)
+                && selection.getEnd() == Math.max(caret,anchor)) {
+            return;
+        }
+        // resizeSelection will fire a caret update which can be ignored.
+        skipCaretUpdate(textArea);
+        textArea.resizeSelection(anchor, caret, 0,
+                        textArea.isRectangularSelectionEnabled());
+    }
+
+    /**
+     *  Update anchor position or remove anchor if its position was removed.
+     *
+     *  If the the buffer has no anchor left in any text areas remove this
+     *  listener. Update the status bar widget in case the anchor was removed.
+     */
+    private static void handlePreContentRemoved(JEditBuffer buffer,
+                                                int offset, int length) {
+        anchorMap.remove(buffer, offset, length);
+        if(!anchorMap.contains(buffer)) {
+            bufferHandler.removeFrom(buffer);
+        }
+        StatusBarWidgetManager.updateAllWidgets();
+    }
+
+    /** Add or remove the caret listener of the edit panes text area and update
+     *  the status bar widget. */
+    @EditBus.EBHandler
     public void handleEditPaneUpdate(EditPaneUpdate updateMessage) {
         if(EditPaneUpdate.BUFFER_CHANGED.equals(updateMessage.getWhat())) {
             EditPane editPane = updateMessage.getEditPane();
@@ -119,15 +143,17 @@ public class AnchoredSelectionPlugin extends EditPlugin {
         }
     }
 
-    @EBHandler
+    /** Update the status bar widget.*/
+    @EditBus.EBHandler
     public void handleViewUpdate(ViewUpdate updateMessage) {
         if(ViewUpdate.EDIT_PANE_CHANGED.equals(updateMessage.getWhat())) {
             View view = updateMessage.getView();
-            StatusBarWidgetManager.updateWidget(view, hasAnchor(view.getTextArea()));
+            StatusBarWidgetManager.updateWidget(view,
+                                                hasAnchor(view.getTextArea()));
         }
     }
 
-    @EBHandler
+    @EditBus.EBHandler
     public void handlePropertiesChanged(PropertiesChanged changedMessage) {
         // jEdit sends this with a source null after the options dialog closes.
         if(changedMessage.getSource() == null) {
@@ -138,14 +164,27 @@ public class AnchoredSelectionPlugin extends EditPlugin {
 
     // {{{ Interface
 
+    /** Skip the next caret update of textArea. */
     static void skipCaretUpdate(TextArea textArea) {
         skipCaretUpdate.add(textArea);
     }
 
+    /** Whether the current buffer of textArea has an achor set, i.e. whether
+     *  anchored selection mode is enabled. */
     static boolean hasAnchor(TextArea textArea) {
         return anchorMap.contains(textArea);
     }
 
+    /**
+     *  Start anchored selection mode.
+     *
+     *  Set the current caret position as the anchor position. If the caret is
+     *  at a selection make an educated guess where the selection start is
+     *  (whether before or after the caret) and use it as the anchor position.
+     *
+     *  Install listeners (if not already installed) and update the status bar
+     *  widget.
+     */
     static void dropAnchor(View view) {
         TextArea textArea = view.getTextArea();
         int caret = textArea.getCaretPosition();
@@ -163,6 +202,13 @@ public class AnchoredSelectionPlugin extends EditPlugin {
         StatusBarWidgetManager.updateWidget(view, true);
     }
 
+    /**
+     *  End anchored selection mode.
+     *
+     *  Forget the anchor position and remove listeners (keep the buffer
+     *  listener if the buffer has an anchor in another text area) and update
+     *  the status bar widget.
+     */
     static void raiseAnchor(View view) {
         TextArea textArea = view.getTextArea();
         JEditBuffer buffer = textArea.getBuffer();
